8c5a544065f68f1f4e0bee429e46cc4c54f78c70
Tony Costa <tonyblack10@gmail.com>
2025-04-18 19:02:06 -0300

    adjusting config

--- device-management/src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
+++ device-management/src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
@@ -3,6 +3,8 @@ package com.algaworks.algasensors.device.management.api.controller;
 import com.algaworks.algasensors.device.management.api.model.SensorInput;
 import com.algaworks.algasensors.device.management.common.IdGenerator;
 import com.algaworks.algasensors.device.management.domain.model.Sensor;
+import com.algaworks.algasensors.device.management.domain.model.SensorId;
+import com.algaworks.algasensors.device.management.domain.repository.SensorRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,11 +12,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/sensors")
 public class SensorController {
 
+    private final SensorRepository sensorRepository;
+
+    public SensorController(SensorRepository sensorRepository) {
+        this.sensorRepository = sensorRepository;
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Sensor create(@RequestBody SensorInput sensorInput) {
-        return Sensor.builder()
-                .id(IdGenerator.generateTSID())
+        var sensor = Sensor.builder()
+                .id(new SensorId(IdGenerator.generateTSID()))
                 .name(sensorInput.name())
                 .ip(sensorInput.ip())
                 .location(sensorInput.location())
@@ -22,6 +30,8 @@ public class SensorController {
                 .model(sensorInput.model())
                 .enabled(false)
                 .build();
+
+        return sensorRepository.saveAndFlush(sensor);
     }
 
 }
